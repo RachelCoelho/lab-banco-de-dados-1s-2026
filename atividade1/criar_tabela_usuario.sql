f6ca63390777e3c338f4a6eff91a450a146a1937
Rachel Coelho <163034541+RachelCoelho@users.noreply.github.com>
2026-03-11 20:55:54 -0300

    Update criar_tabela_usuario.sql

--- atividade1/criar_tabela_usuario.sql
+++ atividade1/criar_tabela_usuario.sql
@@ -1,1 +1,6 @@
-
+create table usuario (
+	id_usuario int primary key auto_increment not null,
+    nome varchar(255) not null,
+    email varchar(128) not null unique,
+    data_cadastro date default(current_date) not null
+);
