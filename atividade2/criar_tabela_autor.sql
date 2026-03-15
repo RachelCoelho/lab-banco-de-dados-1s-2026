f9d69393a7b837698f2f4e6815dd845854e364b5
Rachel Coelho <163034541+RachelCoelho@users.noreply.github.com>
2026-03-15 17:39:45 -0300

    Update criar_tabela_autor.sql

--- atividade2/criar_tabela_autor.sql
+++ atividade2/criar_tabela_autor.sql
@@ -1,1 +1,9 @@
-
+CREATE TABLE Autor (
+    id_autor INT AUTO_INCREMENT PRIMARY KEY,
+    nome VARCHAR(255) NOT NULL UNIQUE,
+    ano_nascimento YEAR,
+    ano_morte YEAR,
+    apresentacao TEXT,
+    data_cadastro TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+    data_atualizacao TIMESTAMP DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP
+);
