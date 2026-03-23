f428882c865eb1bb4f7622a8413c1e47eb7f300e
Rachel Coelho <163034541+RachelCoelho@users.noreply.github.com>
2026-03-23 11:06:08 -0300

    Update script_bd_biblioteca_pessoal_v1.sql

--- atividade3/script_bd_biblioteca_pessoal_v1.sql
+++ atividade3/script_bd_biblioteca_pessoal_v1.sql
@@ -1,1 +1,59 @@
+/* Criação do Banco de Dados */
+CREATE DATABASE biblioteca_pessoal;
+USE biblioteca_pessoal;
 
+/* Tabela Usuario */
+CREATE TABLE Usuario (
+    id_usuario INT AUTO_INCREMENT PRIMARY KEY,
+    nome VARCHAR(255) NOT NULL,
+    email VARCHAR(255) NOT NULL UNIQUE,
+    senha VARCHAR(255) NOT NULL,
+    data_cadastro TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+    data_atualizacao TIMESTAMP DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP
+);
+
+/* Tabela Categoria */
+CREATE TABLE Categoria (
+    id_category INT AUTO_INCREMENT PRIMARY KEY,
+    nome VARCHAR(128) NOT NULL UNIQUE,
+    descricao TEXT,
+    data_cadastro TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+    data_atualizacao TIMESTAMP DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP
+);
+
+/* Tabela Editora */
+CREATE TABLE Editora (
+    id_editora INT AUTO_INCREMENT PRIMARY KEY,
+    nome VARCHAR(255) NOT NULL UNIQUE,
+    cidade VARCHAR(64),
+    estado VARCHAR(64),
+    pais VARCHAR(64),
+    data_cadastro TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+    data_atualizacao TIMESTAMP DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP
+);
+
+/* Tabela Autor */
+CREATE TABLE Autor (
+    id_autor INT AUTO_INCREMENT PRIMARY KEY,
+    nome VARCHAR(255) NOT NULL UNIQUE,
+    ano_nascimento YEAR,
+    ano_morte YEAR,
+    apresentacao TEXT,
+    data_cadastro TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+    data_atualizacao TIMESTAMP DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP
+);
+
+/* Tabela Livro */
+CREATE TABLE Livro (
+    id_livro INT AUTO_INCREMENT PRIMARY KEY,
+    id_usuario INT NOT NULL,
+    id_autor INT NOT NULL,
+    id_editora INT NOT NULL,
+    id_categoria INT NOT NULL,
+    titulo VARCHAR(255) NOT NULL,
+    sinopse TEXT,
+    ano_publicacao YEAR,
+    lido BOOLEAN DEFAULT FALSE,
+    data_cadastro TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+    data_atualizacao TIMESTAMP DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP
+);
